e3353b75d82539cd50fb25599c59745c6d5097d1
M.Umais Adeed <umaisadeed910@gmail.com>
2024-11-11 17:02:08 +0500

    created this simple program

---
+++ band_name _generator.py
@@ -0,0 +1,5 @@
+print("welcome to the band name generatore")
+city=input("please enter the name of the city where were you born")
+pet=input("please enter the name of your pet here")
+print("the name of your band is", city+pet)
+
