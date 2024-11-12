40777fb2d661987a8c8d71d44e721f2f0bd7e838
M.Umais Adeed <umaisadeed910@gmail.com>
2024-11-12 19:49:27 +0500

    Added tip calculation logic

--- tim_calculator.py
+++ tim_calculator.py
@@ -2,3 +2,8 @@
 print("Welcome to the Tip Calculator")
 total = float(input("What was the total bill? "))
 percentage = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+
+# Tip Calculation
+inc = percentage / 100
+per = total * inc
+bill_total = total + per
