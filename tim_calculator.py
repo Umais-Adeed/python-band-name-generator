8c8ade2a32a6acda15970c05d05eacc2a623bc4e
M.Umais Adeed <umaisadeed910@gmail.com>
2024-11-12 19:49:06 +0500

    Initial setup and user prompts for total bill and tip percentage

---
+++ tim_calculator.py
@@ -0,0 +1,4 @@
+# Initial Setup and User Prompts
+print("Welcome to the Tip Calculator")
+total = float(input("What was the total bill? "))
+percentage = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
